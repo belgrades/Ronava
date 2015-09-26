3ebc1657e5a866bc403920009bac561e0c161352
Fernando Crema <fcremaldc@gmail.com>
2015-09-26 01:48:33 -0430

    Create setup.py

---
+++ setup/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import py2exe
+
+Mydata_files = [('images', ['C:\\Users\\Crema\\PycharmProjects\\Ronava\\LogoRonava.png'])]
+
+setup(
+    windows=[{
+        "script": "ronava.py",
+         "icon_resources": [(0, "ronava.ico")]
+    }],
+    data_files=Mydata_files,
+    options={"py2exe": {
+                "includes": ["easygui", "os", "sys", "openpyxl", "warnings"]
+             }}
+    )
